34190aa8f5110417695c6c473143ddec0f1952f6
Antonio Dias <antonioisaacvd@gmail.com>
2026-09-19 10:49:35 -0300

    fix: correct ProjectResponse record name

--- src/main/java/com/antoniodias/tasklist/project/dto/ProjectResponse.java
+++ src/main/java/com/antoniodias/tasklist/project/dto/ProjectResponse.java
@@ -5,7 +5,7 @@ import com.antoniodias.tasklist.project.entity.Project;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-public record simProjectResponse(UUID id, String name, String description, LocalDateTime createdAt) {
+public record ProjectResponse(UUID id, String name, String description, LocalDateTime createdAt) {
 
     public static ProjectResponse from(Project project) {
         return new ProjectResponse(
